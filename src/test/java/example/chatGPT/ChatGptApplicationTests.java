bf07a7dae2fa353c0246b67efcf1aa1a3dde0000
yeonwoo-Kim <olenexxwxxxy@gmail.com>
2024-05-20 22:24:28 +0900

    first commit

---
+++ src/test/java/example/chatGPT/ChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package example.chatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
